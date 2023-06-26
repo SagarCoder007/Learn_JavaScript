38ca79daa558773f608a81460b946481c5ad2910
SagarCoder007 <sagarvarma10001@gmail.com>
2023-06-26 16:51:45 +0530

    done with primitives data types and objects

---
+++ 3.primitive_objects/main.js
@@ -0,0 +1,24 @@
+//there are seven types of data types
+
+ let a= 45;
+ let b= null;
+ let c= "so basically i am a hacker";
+ let d= BigInt("543");
+ let e= Symbol("i am a nice symbol");
+ let f= true;  // it can br true and false
+ let g= undefined;
+
+console.log(a,b,c,d.e,f,g)
+
+console.log(typeof(d)) //typepf keyword help to identify the data type of the give variable
+
+// objects in javascript
+
+const item = {
+    "sagar": true,
+    "rohan": undefined,
+    "mahesh": false,
+    "manisha": 68
+}
+
+console.log(item)
