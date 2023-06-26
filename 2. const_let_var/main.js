b8bc75e9a7dae9c8fba7665e2a0618e7122b1f45
SagarCoder007 <sagarvarma10001@gmail.com>
2023-06-26 15:33:27 +0530

    done with const let var

---
+++ 2. const_let_var/main.js
@@ -0,0 +1,15 @@
+//so there are three main types const let var 
+//most of us we use let and const
+
+let a =45
+let b=346
+console.log("the sum of a+b is :",a+b)
+
+const k= 46    // so the value of k will be the same constatnt 46 
+console.log(k)   //printing the value of k
+
+k=30    //so if yourll try to change it will not change because it is costant
+console.log(k)
+
+var h="my channel name is sagarcoder Do visit"  //it is one of the least use type
+console.log(h)
