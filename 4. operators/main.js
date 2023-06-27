368c2ab8b3c53c022339750a26fe2d690bd644be
SagarCoder007 <sagarvarma10001@gmail.com>
2023-06-27 15:35:55 +0530

    done with operators

---
+++ 4. operators/main.js
@@ -0,0 +1,32 @@
+//operators in js
+
+// arithmetic operator
+
+let a =35
+let b =3
+
+console.log("the value of a+b is",a+b)
+console.log("the value of a-b is",a-b)
+console.log("the value of a*b is",a*b)
+console.log("the value of a/b is",a/b)
+console.log("the value of ++a is",++a)
+console.log("the value of a++ is",a++)
+console.log("the value of --a is",--a)
+console.log("the value of a-- is",a--)
+console.log("the value of a**b is",a**b)
+
+// Assignment operato
+let ass =1
+ass += 46
+console.log("the value of ass=ass+value",ass)
+ass -= 35
+console.log("the value of ass=ass-value",ass)
+
+//comparison operators
+let com1=34
+let com2="34"
+console.log("com1==com2",com1==com2)
+console.log("com1!=com2",com1!=com2)
+console.log("com1===com2",com1===com2)
+console.log("com1!==com2",com1!==com2)
+
