186d0a1d047e71f09cce41f297ee583ab9cbf70e
SagarCoder007 <sagarvarma10001@gmail.com>
2023-06-27 16:33:11 +0530

    conditional statements

---
+++ 5. Conditional_Expressions/main.js
@@ -0,0 +1,13 @@
+
+console.log("Conditional Expression in js  ")
+
+let a =prompt("whats your age??")
+
+if(a>=0 && a<=18){
+    alert("you are not able to drive you r not of 18+")
+}
+
+else if(a>18){
+ alert("now you are aligible for driving")
+}
+
