f92bfee3abf37256cccd522dfa318560add6b7c5
SagarCoder007 <sagarvarma10001@gmail.com>
2023-07-27 16:06:47 +0530

    done with array methods

---
+++ 9. array_methods/main.js
@@ -0,0 +1,20 @@
+let arr=[1,3,4,6,8,9,63,8]
+
+// console.log(arr.length)
+// arr.pop() removes from last 
+//  arr.push() all at last 
+// arr.shift() remove from start 
+// arr.unshift(5) add at start
+
+//  console.log(arr.join())
+
+// delete arr[6]
+// console.log(arr)
+
+// console.log(arr.splice(2,4))
+
+// console.log(arr.slice(2,4))
+
+// console.log(arr.reverse())
+
+// console.log(arr.sort())
