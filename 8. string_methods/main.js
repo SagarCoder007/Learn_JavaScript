82a009fb44d4562e45eba5d25c9874cea83b19ec
SagarCoder007 <sagarvarma10001@gmail.com>
2023-07-27 15:29:04 +0530

    done with str

---
+++ 8. string_methods/main.js
@@ -0,0 +1,36 @@
+
+// strung methods
+
+
+let a="mahesh"
+
+let b= "sagar"
+// console.log(a.length)
+
+// console.log(a.slice(2,4))
+
+// console.log(a.substring(2))
+
+// console.log(a.substr(7,6))
+
+// console.log(a.replace("mahesh","suraj"))
+
+// console.log(a.replaceAll("mahesh","suraj"))
+
+// console.log(a.toUpperCase())
+
+// console.log(a.toLowerCase())
+
+// let c= a.concat("+",b)
+
+// console.log(a.trim())
+
+//  console.log(a.padStart(2,0))
+
+// console.log(a.charAt(2))
+
+// console.log(a.charCodeAt(2))
+
+// console.log(a.split())
+
+
