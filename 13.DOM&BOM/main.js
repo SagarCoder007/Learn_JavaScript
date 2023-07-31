d207c12524f6368274c5c4362a2c575ad3a9b3c1
SagarCoder007 <sagarvarma10001@gmail.com>
2023-07-31 20:24:43 +0530

    donw with dom

---
+++ 13.DOM&BOM/main.js
@@ -0,0 +1,13 @@
+//DOM is document object model
+//BOM is browser object model
+
+console.log(window)
+window.console.log(window)
+/* so basically the main is window
+under window BOM ,DOM and havascript core comes..
+*/
+
+// with the help of this we can design the
+// html page in js 
+
+document.body.style.backgroundColor ="beige"
