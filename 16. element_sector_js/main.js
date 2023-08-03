1339497188b26f46093e1b587f730cb6cbeb34dd
SagarCoder007 <sagarvarma10001@gmail.com>
2023-08-03 17:05:07 +0530

    done with it

---
+++ 16. element_sector_js/main.js
@@ -0,0 +1,27 @@
+let h1 = document.querySelector(".title")
+let input = document.querySelector("#input")
+let btn = document.querySelector(".btn")
+
+// let arr=[]
+// Array.from(h1).forEach(function(e){
+// //   console.log(e)
+// //  e.style.fontWeight="bold"
+// e.style.color="red"
+// arr.push(e)
+// })
+// arr[0].style.color="yellow"
+// arr[1].style.color="pink"
+
+// console.log(h1)
+// btn.addEventListener("click",function(){
+//     console.log("hii")
+// })
+
+// btn.addEventListener("click",function(){
+   
+//     let value =input.value
+//     h1.innerHTML= `welcome to ${value}`
+//     input.value=""
+// })
+
+console.log(`sagar varma ${h1.innerHTML}`)
